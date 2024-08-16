f7116a73ecdd8f1ca9231aa50c1287cb2f71c52e
jly02 <95561288+jly02@users.noreply.github.com>
2024-08-16 11:38:48 -0700

    Update move.py

--- move.py
+++ move.py
@@ -2,19 +2,9 @@ import time
 from chassis_control import *
 
 def main():
-    v_s = [-10, 10, 10, -10]
-
     chassis = Chassis()
 
-    """
-    direction = 0
-    for i in range(9):
-        theta = direction + i * 45
-        chassis.set_velocity((10, theta), 0)
-        time.sleep(1)
-    """
-
-    chassis.set_velocity((-50, 90), 0)
+    chassis.set_velocity(-50, 90, 0)
     time.sleep(1)
 
     chassis.stop()
